97b1e877cd44da988bc7aaac2b28b8bcec81af9f
Mohit Singh Bikawat <111340432+mohitsingh13@users.noreply.github.com>
2024-06-29 19:57:45 +0530

    Initial commit

---
+++ MotoMarvelStudio/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer_container">
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
+          <a href="#">Helmets</a>
+            <a href="#">Riding Gears</a>
+            <a href="#">Parts</a>
+            <a href="#">Power Parts</a>
+            <a href="#">Exausts</a>
+          <a href="#">Studio Insider</a>
+        </div>
+
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
+          <a href="#">Helmets</a>
+            <a href="#">Riding Gears</a>
+            <a href="#">Parts</a>
+            <a href="#">Power Parts</a>
+            <a href="#">Exausts</a>
+          <a href="#">Studio Insider</a>
+        </div>
+
+        <div className="footer_column">
+          <h3>ONLINE SHOPPING</h3>
+
+          <a href="#">Helmets</a>
+            <a href="#">Riding Gears</a>
+            <a href="#">Parts</a>
+            <a href="#">Power Parts</a>
+            <a href="#">Exausts</a>
+          <a href="#">Studio Insider</a>
+        </div>
+      </div>
+      <hr />
+
+      <div className="copyright">
+        © 2023 www.MotoMarvelStudio.com. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ MotoMarvelStudio/src/routes/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      <h2 className="Profile_css">
+      Moto Store India specialises in alloy wheels, establishing itself as a prominent industry leader in the field for the last years of experience. The dealership boasts a wide range of high-quality alloy wheels, sourced exclusively from renowned and reliable brands.
+      </h2>
+    </div>
+  )
+}
+
+export default Profile
